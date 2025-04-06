31b7062c38164dc9bb2bd7e485b74dc170c8e2d7
adsadilson <adparaguai@gmail.com>
2025-04-06 01:11:06 -0300

    Inclusão do lombok, validation e criação ResourceNotFoundException e StandardError.

---
+++ hd-commons/src/main/java/model/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package model.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
